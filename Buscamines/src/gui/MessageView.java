2df1985793da4e398253786015825a08dfbac2fb
Marc <marcgarnica13@gmail.com>
2015-01-10 00:11:01 +0100

    Commit de prova

--- Buscamines/src/gui/MessageView.java
+++ Buscamines/src/gui/MessageView.java
@@ -22,8 +22,6 @@ public class MessageView extends JFrame {
 		this.text = textToShow;
 		this.previous = prev;
 		this.ctrl = ctrl;
-		
-		
 		setTitle("System Message");
 		setMinimumSize(new Dimension(300,200));
 		setResizable(false);
